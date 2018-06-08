6b3283a9e89166bbc556e2eb3189cd4335757588
Marvin Lin <marvin.lin@cloud-interactive.com>
2018-06-08 16:04:04 +0800

    first commit

---
+++ DynamicTextFieldObjC/DynamicTitleInputView.h
@@ -0,0 +1,13 @@
+//
+//  DynamicTitleInputView.h
+//  DynamicTextFieldObjC
+//
+//  Created by Marvin iOS on 2018/6/8.
+//  Copyright © 2018年 Cloud-SmartGym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DynamicTitleInputView : UIView
+
+@end
---
+++ DynamicTextFieldObjC/DynamicTitleInputView.m
@@ -0,0 +1,21 @@
+//
+//  DynamicTitleInputView.m
+//  DynamicTextFieldObjC
+//
+//  Created by Marvin iOS on 2018/6/8.
+//  Copyright © 2018年 Cloud-SmartGym. All rights reserved.
+//
+
+#import "DynamicTitleInputView.h"
+
+@implementation DynamicTitleInputView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
